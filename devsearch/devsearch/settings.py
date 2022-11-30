04312a36fd225e33557dff46553ed6de41e82598
Kate S <kateshpudejko@gmail.com>
2022-11-30 09:34:24 +0100

    commit 2: created users app, theme applied

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
